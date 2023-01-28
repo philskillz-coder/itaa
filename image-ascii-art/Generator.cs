4ca99dfed1c6e9a5d91b83149ffcbe0223b04303
Philskillz <76884393+philskillz-coder@users.noreply.github.com>
2023-01-28 08:50:37 +0100

    first commit

---
+++ image-ascii-art/Generator.cs
@@ -0,0 +1,150 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+using System.Runtime.Versioning;
+
+namespace image_ascii_art;
+
+[SupportedOSPlatform("windows")]
+internal class Generator
+{
+    public static Bitmap resizeImage(Bitmap imgToResize, Size size)
+    {
+        return new Bitmap(imgToResize, size);
+    }
+
+    public static Bitmap MakeGrayscale3(Bitmap original)
+    {
+        Bitmap newBitmap = new Bitmap(original.Width, original.Height);
+
+        using (Graphics g = Graphics.FromImage(newBitmap))
+        {
+            ColorMatrix colorMatrix = new ColorMatrix(
+               new float[][]
+               {
+                    new float[] {.3f, .3f, .3f, 0, 0},
+                    new float[] {.59f, .59f, .59f, 0, 0},
+                    new float[] {.11f, .11f, .11f, 0, 0},
+                    new float[] {0, 0, 0, 1, 0},
+                    new float[] {0, 0, 0, 0, 1}
+               });
+
+            using (ImageAttributes attributes = new ImageAttributes())
+            {
+                attributes.SetColorMatrix(colorMatrix);
+
+                g.DrawImage(
+                    original, new Rectangle(0, 0, original.Width, original.Height),
+                            0, 0, original.Width, original.Height, GraphicsUnit.Pixel, attributes);
+            }
+        }
+        return newBitmap;
+    }
+
+    public static int mirror(int max, int n)
+    {
+        return -n + max - 1;
+    }
+
+    public static int chunk(int chunks, int n)
+    {
+        return (int)(n / (256.0 / chunks));
+    }
+
+    public static int zero_if_negative(int n)
+    {
+        if (n < 0) return 0;
+        return n;
+    }
+
+    public static Image GenerateImage(
+        string ImagePath,
+        int? Width,
+        string ImageText,
+        int FontSize,
+        int CharacterSpacing,
+        int BrightnessLevels,
+        int MinLetters,
+        int Offset
+    )
+    {
+        Bitmap original = (Bitmap) Image.FromFile(ImagePath);
+        int ResizedWidth = Width ?? original.Width;
+
+        float w_pc = (float) ResizedWidth / original.Width;
+        int ResizedHeight = (int) Math.Floor(original.Height * w_pc);
+
+        int NewWidth = ResizedWidth * CharacterSpacing;
+        int NewHeight = ResizedHeight * CharacterSpacing;
+
+        var b = new Bitmap(1, 1);
+        b.SetPixel(0, 0, Color.White);
+
+        Bitmap @new = new Bitmap(NewWidth, NewHeight);
+        for (int y = 0; y < NewHeight; y++)
+        {
+            for (int x = 0; x < NewWidth; x++)
+            {
+                @new.SetPixel(x, y, Color.White);
+            }
+        }
+
+        var size = new Size(ResizedWidth, ResizedHeight);
+        original = resizeImage(original, size);
+
+        original = MakeGrayscale3(original);
+
+        using Graphics graphics = Graphics.FromImage(@new);
+        using Font font = new Font("arial.ttf", FontSize);
+
+        int current_character = 0;
+        for (int y = 0; y < ResizedHeight; y++)
+        {
+            for (int x = 0; x < ResizedWidth; x++)
+            { 
+                Color pixel = original.GetPixel(x, y);
+                int letter_count = mirror(BrightnessLevels, chunk(BrightnessLevels, (int) (pixel.GetBrightness()*255))) + MinLetters;
+                int offset_bounds = Offset * letter_count;
+                string current_char = ImageText[current_character].ToString();
+
+                if (letter_count == 1)
+                {
+                    graphics.DrawString(
+                        current_char,
+                        font,
+                        Brushes.Black,
+                        new Point(
+                            x * CharacterSpacing,
+                            y * CharacterSpacing
+                        )
+                    );
+                } else
+                {
+                    for (int i = 0; i < letter_count; i++)
+                    {
+                        Random rand0 = new Random();
+                        Random rand1 = new Random();
+                        int offset_x = (rand0.Next(2 * offset_bounds) - offset_bounds) / 2;
+                        int offset_y = (rand1.Next(2 * offset_bounds) - offset_bounds) / 2;
+
+                        graphics.DrawString(
+                            current_char,
+                            font,
+                            new SolidBrush(Color.Black),
+                            new Point(
+                                zero_if_negative(x * CharacterSpacing + offset_x),
+                                zero_if_negative(y * CharacterSpacing + offset_y)
+                            )
+                        );
+                    }
+                }
+                current_character++;
+                current_character %= ImageText.Length;
+
+            }
+        }
+        
+
+        return @new;
+    }
+
+}
---
+++ image-ascii-art/Program.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Runtime.Versioning;
+using Aspose.Imaging.ImageOptions;
+using CommandLine;
+using static image_ascii_art.Program;
+
+namespace image_ascii_art;
+
+class Program
+{
+    public class Options
+    {
+        [Option('i', "image", Required = true, HelpText = "The image to process")]
+        public string ImagePath { get; set; }
+
+        [Option('o', "output", Required = true, HelpText = "The output path of the image")]
+        public string OutputPath { get; set; }
+
+        [Option('t', "text", Required = false, Default = "theskz4evr", HelpText = "The text to write on the image")]
+        public string ImageText { get; set; }
+
+        [Option("resized-width", Required = false, HelpText = "The width to resize the image before processing")]
+        public int? ResizedWidth { get; set; }
+
+        [Option("font-size", Required = false, Default = 10, HelpText = "The text font size")]
+        public int FontSize { get; set; }
+
+        [Option("character-spacing", Required = false, Default = 8, HelpText = "How many pixels to space the characters apart")]
+        public int CharacterSpacing { get; set; }
+
+        [Option("brightness-levels", Required = false, Default = 8, HelpText = "How many levels of brightness there are")]
+        public int BrightnessLevels { get; set; }
+
+        [Option("min-letters", Required = false, Default = 1, HelpText = "The minimum number of letters per pixel")]
+        public int MinLetters { get; set; }
+
+        [Option("offset", Required = false, Default = 2, HelpText = "The offset (in pixels) per letter")]
+        public int Offset { get; set; }
+
+    }
+
+    [SupportedOSPlatform("windows")]
+    static void Main(string[] args)
+    {
+
+        Console.ForegroundColor = ConsoleColor.Blue;
+
+        Console.WriteLine("IIIIIIIIIITTTTTTTTTTTTTTTTTTTTTTT         AAA                              AAA               ");
+        Console.WriteLine("I::::::::IT:::::::::::::::::::::T        A:::A                            A:::A              ");
+        Console.WriteLine("I::::::::IT:::::::::::::::::::::T       A:::::A                          A:::::A             ");
+        Console.WriteLine("II::::::IIT:::::TT:::::::TT:::::T      A:::::::A                        A:::::::A            ");
+        Console.WriteLine("  I::::I  TTTTTT  T:::::T  TTTTTT     A:::::::::A                      A:::::::::A           ");
+        Console.WriteLine("  I::::I          T:::::T            A:::::A:::::A                    A:::::A:::::A          ");
+        Console.WriteLine("  I::::I          T:::::T           A:::::A A:::::A                  A:::::A A:::::A         ");
+        Console.WriteLine("  I::::I          T:::::T          A:::::A   A:::::A                A:::::A   A:::::A        ");
+        Console.WriteLine("  I::::I          T:::::T         A:::::A     A:::::A              A:::::A     A:::::A       ");
+        Console.WriteLine("  I::::I          T:::::T        A:::::AAAAAAAAA:::::A            A:::::AAAAAAAAA:::::A      ");
+        Console.WriteLine("  I::::I          T:::::T       A:::::::::::::::::::::A          A:::::::::::::::::::::A     ");
+        Console.WriteLine("  I::::I          T:::::T      A:::::AAAAAAAAAAAAA:::::A        A:::::AAAAAAAAAAAAA:::::A    ");
+        Console.WriteLine("II::::::II      TT:::::::TT   A:::::A             A:::::A      A:::::A             A:::::A   ");
+        Console.WriteLine("I::::::::I      T:::::::::T  A:::::A               A:::::A    A:::::A               A:::::A  ");
+        Console.WriteLine("I::::::::I      T:::::::::T A:::::A                 A:::::A  A:::::A                 A:::::A ");
+        Console.WriteLine("IIIIIIIIII      TTTTTTTTTTTAAAAAAA                   AAAAAAAAAAAAAA                   AAAAAAA");
+
+        Console.ForegroundColor = ConsoleColor.Magenta;
+        Console.WriteLine("                                  [I]mage [t]o [a]scii [a]rt                                  ");
+        Console.WriteLine("                                   Made by Philskillz_#0266                                   ");
+        Console.WriteLine("                                          theskz.dev                                          ");
+
+        Console.ForegroundColor = ConsoleColor.White;
+
+        Parser.Default.ParseArguments<Options>(args).WithParsed<Options>(options =>
+        {
+
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine($"Selected image: {options.ImagePath}");
+            Console.WriteLine($"Using text: {options.ImageText}");
+            Console.WriteLine($"Resized image: {options.ResizedWidth.ToString() ?? "<original>"} pixels");
+            Console.ForegroundColor = ConsoleColor.White;
+
+            var watch = new System.Diagnostics.Stopwatch();
+            watch.Start();
+            using (Image image = Generator.GenerateImage(
+                ImagePath: options.ImagePath,
+                Width: options.ResizedWidth,
+                ImageText: options.ImageText,
+                FontSize: options.FontSize,
+                CharacterSpacing: options.CharacterSpacing,
+                BrightnessLevels: options.BrightnessLevels,
+                MinLetters: options.MinLetters,
+                Offset: options.Offset
+                )
+            )
+            {
+                using (Stream s = new MemoryStream())
+                {
+                    image.Save(s, ImageFormat.Png);
+                    s.Seek(0, SeekOrigin.Begin);
+
+                    using (Aspose.Imaging.Image im = Aspose.Imaging.Image.Load(s))
+                    {
+                        PngOptions o = new PngOptions();
+                        o.CompressionLevel = 9;
+                        im.Save(options.OutputPath, o);
+                    }
+                }
+            }
+            watch.Stop();
+
+            Console.WriteLine();
+            Console.WriteLine("#############################################################################################");
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"Image generated in {watch.Elapsed.TotalSeconds.ToString("0.##")} seconds");
+            Console.WriteLine($"Image saved as {options.OutputPath}");
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine();
+            Console.WriteLine("#############################################################################################");
+        });
+
+        //using (Image image = Generator.GenerateImage(
+        //    ImagePath: "C:\\Users\\Philskillz\\PycharmProjects\\image-text-art\\img.png",
+        //    Width: 500,
+        //    ImageText: "theskz",
+        //    FontSize: 10,
+        //    CharacterSpacing: 8,
+        //    BrightnessLevels: 8,
+        //    MinLetters: 1,
+        //    Offset: 4
+        //    )
+        //)
+        //{
+        //    image.Save(
+        //        "C:\\Users\\Philskillz\\source\\repos\\image-ascii-art\\image-ascii-art\\save.png"
+        //    );
+        //}
+
+
+    }
+}
